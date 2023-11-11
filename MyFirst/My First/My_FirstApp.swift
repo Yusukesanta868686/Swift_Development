a2f7086b59d151c6ffde9782e5bc0aa6e881a88a
Yusukesanta868686 <146314432+Yusukesanta868686@users.noreply.github.com>
2023-11-12 00:36:46 +0900

    整理

---
+++ MyFirst/My First/My_FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FirstApp.swift
+//  My First
+//
+//  Created by 平田佑亮 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct My_FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
